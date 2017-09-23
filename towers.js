ebfc9563b365ef8ea8623799cd06392015d6376f
RoseCrime <31193383+RoseCrime@users.noreply.github.com>
2017-09-23 12:09:18 +0300

    Create new file

---
+++ towers.js
@@ -0,0 +1,70 @@
+var myTowers = [];
+var enemyTowers = [];
+
+function Tower(x, y) {
+    this.x = x;
+    this.y = y;
+    this.towerSize = 25;
+    this.health = 50000;
+
+    this.healthbarSize = 50;
+    map(this.health, 0, 50000, 0, 50);
+
+    this.range = 200;
+    this.damage = 50;
+    this.min = null;
+    this.minDist = 200;
+    this.closestEnemy = null;
+
+    this.refresh = function() {
+        rectMode(CENTER);
+        stroke(0);
+        fill(255);
+        ellipse(this.x, this.y, this.towerSize, this.towerSize);
+    }
+    this.healthDisplay = function() {
+        stroke(0);
+        fill(255, 0, 0);
+        rect(this.x, this.y + this.towerSize, this.healthbarSize, 10);
+    }
+    this.dealDamage = function() {
+        line(this.x, this.y, this.closestEnemy.x, this.closestEnemy.y);
+        this.closestEnemy.health -= this.damage;
+    }
+    this.resetTarget = function() {
+        this.closestEnemy = 0;
+        this.minDist = 200;
+        this.min = 0;
+    }
+}
+
+function towerMaker() {
+    myTowers.push(new Tower(200, 150 - 1 / 2 * 25 + 3));
+    myTowers.push(new Tower(200, 50 + 1 / 2 * 25 - 3));
+
+    enemyTowers.push(new Tower(600, 150 - 1 / 2 * 25 + 3));
+    enemyTowers.push(new Tower(600, 50 + 1 / 2 * 25 - 3));
+
+}
+
+function towerRefresher() {
+
+    for (i = 0; i < myTowers.length; i++) {
+        myTowers[i].refresh();
+        myTowers[i].healthDisplay();
+        myTowers[i].healthbarSize = map(myTowers[i].health, 0, 50000, 0, 50);
+        if (myTowers[0].health <= 0) {
+            myTowers.splice(i, 1);
+            enemyCoins += 1000;
+        }
+    }
+    for (i = 0; i < enemyTowers.length; i++) {
+        enemyTowers[i].refresh();
+        enemyTowers[i].healthDisplay();
+        enemyTowers[i].healthbarSize = map(enemyTowers[i].health, 0, 50000, 0, 50);
+        if (enemyTowers[i].health <= 0) {
+            enemyTowers.splice(i, 1);
+            coins += 1000;
+        }
+    }
+}
